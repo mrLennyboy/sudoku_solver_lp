aa681ddfad9c45f365d50f343410cbd4aabb8137
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2022-06-17 15:43:54 -0700

    Move wrapped functions out of main

--- main.py
+++ main.py
@@ -10,31 +10,6 @@
 # Import PuLP, python linear programming modeller function as alias plp
 import pulp as plp
 
-# Step 1: Define the Linear Programming problem (variable to contain problem data)
-# Use LpVariable() to create new variables. ie variable 0 <= x <= 3 --> x = LpVariable("x", 0, 3)
-sudoku_problem = plp.LpVariable("Sudoku_problem")
-
-# Step 2: Set the objective function
-# An objective function is a linear function whose values generally need to be either min or max based on
-# the problem to be solved.
-# With sudoku there is no solution that is better than another solution, since a sudoku's solution isn't 
-# min max'd. The sudoku's solution is defined by it completing the constraints of the problem, 1-9 of row,
-# column, and grids. In some cases diagonal to if it is also a diagonal sudoku.
-# Set dummy objective
-objective = plp.lpSum(0)
-sudoku_problem.setObjective(objective)
-
-# Step 3: Define the Linear Programming problem
-# Sudoku grid is 9 x 9 grid and consists of 81 cells. Each cell can take a value between 1 - 9.
-# Since each cell can only have one value set bool value.
-rows = range(0, 9)
-cols = range(0, 9)
-grids = range(0, 9)
-values = range(1, 10)
-
-# Decision Variable/Target Variable
-grid_variables = plp.LpVariable.dicts("grid_value", (rows,cols,values), cat = 'Binary')
-
 # wrap loops for diagonal constraints within a functions
 def add_default_sudoku_constraints(sudoku_problem, grid_variables, rows, cols, grids, values):
   # Step 4: Set the constraints
@@ -95,33 +70,60 @@ def add_prefilled_constraints(sudoku_problem, input_sudoku, grid_variables, rows
         rhs=input_sudoku[row][col], 
         name=f"constraint_prefilled_{row}_{col}"))
 
-# Step 5: Solve the Sudoku puzzle
-# After the Objective function, decision variables, and constraints are set the sudoku solver can be invoked
-sudoku_problem.solve()
+def solve_sudoku(input_sudoku, diagonal = False):
+  # Step 1: Define the Linear Programming problem (variable to contain problem data)
+  # Use LpVariable() to create new variables. ie variable 0 <= x <= 3 --> x = LpVariable("x", 0, 3)
+  sudoku_problem = plp.LpVariable("Sudoku_problem")
 
-# Step 6: Check if an optimal result is found
-print(f'Solution Status = {plp.LpStatus[sudoku_problem.status]}')
+  # Step 2: Set the objective function
+  # An objective function is a linear function whose values generally need to be either min or max based on
+  # the problem to be solved.
+  # With sudoku there is no solution that is better than another solution, since a sudoku's solution isn't 
+  # min max'd. The sudoku's solution is defined by it completing the constraints of the problem, 1-9 of row,
+  # column, and grids. In some cases diagonal to if it is also a diagonal sudoku.
+  # Set dummy objective
+  objective = plp.lpSum(0)
+  sudoku_problem.setObjective(objective)
 
-# Code to extract the final solution grid
-solution = [[0 for col in cols] for row in rows]
-grid_list = []
-for row in rows:
-  for col in cols:
-    for value in values:
-      if plp.value(grid_variables[row][col][value]):
-        solution[row][col] = value 
+  # Step 3: Define the Linear Programming problem
+  # Sudoku grid is 9 x 9 grid and consists of 81 cells. Each cell can take a value between 1 - 9.
+  # Since each cell can only have one value set bool value.
+  rows = range(0, 9)
+  cols = range(0, 9)
+  grids = range(0, 9)
+  values = range(1, 10)
 
-# Step 7: Print the result
-# Print the final solution as a grid
-print(f"\nFinal result:")
+  # Decision Variable/Target Variable
+  grid_variables = plp.LpVariable.dicts("grid_value", (rows,cols,values), cat = 'Binary')
 
-print("\n\n+ ----------- + ----------- + ----------- +",end="")
-for row in rows:
-  print("\n",end="\n|  ")
-  for col in cols:
-    num_end = "  |  " if ((col+1)%3 == 0) else "   "
-    print(solution[row][col],end=num_end)
 
-  if ((row+1)%3 == 0):
-    print("\n\n+ ----------- + ----------- + ----------- +",end="")
+  # Step 5: Solve the Sudoku puzzle
+  # After the Objective function, decision variables, and constraints are set the sudoku solver can be invoked
+  sudoku_problem.solve()
+
+  # Step 6: Check if an optimal result is found
+  print(f'Solution Status = {plp.LpStatus[sudoku_problem.status]}')
+
+  # Code to extract the final solution grid
+  solution = [[0 for col in cols] for row in rows]
+  grid_list = []
+  for row in rows:
+    for col in cols:
+      for value in values:
+        if plp.value(grid_variables[row][col][value]):
+          solution[row][col] = value 
+
+  # Step 7: Print the result
+  # Print the final solution as a grid
+  print(f"\nFinal result:")
+
+  print("\n\n+ ----------- + ----------- + ----------- +",end="")
+  for row in rows:
+    print("\n",end="\n|  ")
+    for col in cols:
+      num_end = "  |  " if ((col+1)%3 == 0) else "   "
+      print(solution[row][col],end=num_end)
+
+    if ((row+1)%3 == 0):
+      print("\n\n+ ----------- + ----------- + ----------- +",end="")
 
