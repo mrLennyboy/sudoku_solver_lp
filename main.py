55c087b8d4c4d08818a27a7089a8433c8ce70ae9
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2022-06-17 12:57:34 -0700

    Added step 4

--- main.py
+++ main.py
@@ -36,6 +36,50 @@ values = range(1, 10)
 grid_variables = plp.LpVariable.dicts("grid_value", (rows,cols,values), cat = 'Binary')
 
 # Step 4: Set the constraints
+# Add generic constraints to solve the sudoku problem. 
+# Constraint 1: Each cell should be filled with a single value between 1 and 9.
+# Constraint 2: Each row should contain every number from 1 to 9 once.
+# Constraint 3: Each col should contain every number from 1 to 9 once.
+# Constraint 4: Each 3 x 3 grid should contain every number from 1 to 9 once.
+
+# Constraint 1: Each cell should be filled with a single value between 1 and 9.
+for row in rows:
+  for col in cols:
+    sudoku_problem.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_variables[row][col][value] for value in values]),
+    sense=plp.LpConstraintEQ, rhs=1, name=f"constraint_sum_{row}_{col}"))
+
+# Constraint 2: Each row should contain every number from 1 to 9 once.
+for row in rows:
+  for value in values:
+    sudoku_problem.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_variables[row][col][value]*value for col in cols]),
+    sense=plp.LpConstraintEQ, rhs=value, name=f"constraint_uniq_row_{row}_{value}"))
+
+# Constraint 3: Each col should contain every number from 1 to 9 once.
+for col in cols:
+  for value in values:
+    sudoku_problem.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_variables[row][col][value]*value for row in rows]),
+    sense=plp.LpConstraintEQ, rhs=value, name=f"constraint_uniq_col_{col}_{value}"))
+
+# Constraint 4: Each 3 x 3 grid should contain every number from 1 to 9 once.
+for grid in grids:
+  grid_row  = int(grid/3)
+  grid_col  = int(grid%3)
+
+  for value in values:
+    sudoku_problem.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_variables[grid_row*3+row][grid_col*3+col][value]*value for col in range(0,3) for row in range(0,3)]),
+    sense=plp.LpConstraintEQ, rhs=value, name=f"constraint_uniq_grid_{grid}_{value}"))
+
+# Constraint 5: An additional constraint for diagonal should contain every number from 1 to 9 once.
+# Constraint from top-left to bottom-right
+for value in values:
+  sudoku_problem.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_variables[row][row][value]*value  for row in rows]),
+  sense=plp.LpConstraintEQ, rhs=value, name=f"constraint_uniq_diag1_{value}"))
+
+# Constraint from top-right to bottom-left
+for value in values:
+  sudoku_problem.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_variables[row][len(rows)-row-1][value]*value  for row in rows]),
+  sense=plp.LpConstraintEQ, rhs=value, name=f"constraint_uniq_diag2_{value}"))
+
 # Step 5: Solve the Sudoku puzzle
 # Step 6: Check if an optimal result is found
 # Step 7: Print the result
